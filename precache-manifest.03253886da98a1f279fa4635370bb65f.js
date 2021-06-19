483befbed0547b664d69ec70bf452b7d769cb8e4
prashant-kumar01 <prashantkumar.cse19@chitkarauniversity.edu.in>
2021-06-19 11:49:32 +0530

    Updates

---
+++ precache-manifest.03253886da98a1f279fa4635370bb65f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "34d74dc0dce66802fe7c9bd6577c3790",
+    "url": "/Food-Recipe-Finder/index.html"
+  },
+  {
+    "revision": "1441a8da851b6b486f92",
+    "url": "/Food-Recipe-Finder/static/css/main.8aebae24.chunk.css"
+  },
+  {
+    "revision": "6455f491aa95ecade754",
+    "url": "/Food-Recipe-Finder/static/js/2.2860c516.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Food-Recipe-Finder/static/js/2.2860c516.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1441a8da851b6b486f92",
+    "url": "/Food-Recipe-Finder/static/js/main.3093b92a.chunk.js"
+  },
+  {
+    "revision": "01d5f2869417e6e21b15",
+    "url": "/Food-Recipe-Finder/static/js/runtime-main.f0959545.js"
+  },
+  {
+    "revision": "709b011f8ddde3769bf5bbefcb703db7",
+    "url": "/Food-Recipe-Finder/static/media/bg2.709b011f.jpg"
+  }
+]);
